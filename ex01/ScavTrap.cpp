c510ae458cdf8f1d3434eb83d87125b20a1fc252
ymori <yuu.mori06@gmail.com>
2022-09-21 00:32:15 +0900

    fix: Changes in ClapTrap are reflected in ex01

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -8,27 +8,27 @@ ScavTrap::ScavTrap() : ClapTrap() {
 	_hitPoints = 100;
 	_energyPoints = 50;
 	_attackDamage = 20;
-	std::cout << "default constructor is called in ScavTrap" << std::endl;
+	std::cout << "[default constructor is called in ScavTrap]" << std::endl;
 }
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
 	_hitPoints = 100;
 	_energyPoints = 50;
 	_attackDamage = 20;
-	std::cout << "constructor is called in ScavTrap" << std::endl;
+	std::cout << "[constructor is called in ScavTrap]" << std::endl;
 }
 
 ScavTrap::~ScavTrap() {
-	std::cout << "destructor is called in ScavTrap" << std::endl;
+	std::cout << "[destructor is called in ScavTrap]" << std::endl;
 }
 
-ScavTrap::ScavTrap(const ScavTrap &scavTrap) {
-	std::cout << "copy constructor in ScavTrap" << std::endl;
+ScavTrap::ScavTrap(const ScavTrap &scavTrap) : ClapTrap(scavTrap) {
+	std::cout << "[copy constructor in ScavTrap]" << std::endl;
 	*this = scavTrap;
 }
 
 ScavTrap& ScavTrap::operator=(const ScavTrap &scavTrap) {
-	_name = scavTrap._name;
+	static_cast<std::string>(_name) = scavTrap._name;
 	_hitPoints = scavTrap._hitPoints;
 	_energyPoints = scavTrap._energyPoints;
 	_attackDamage = scavTrap._attackDamage;
@@ -36,14 +36,18 @@ ScavTrap& ScavTrap::operator=(const ScavTrap &scavTrap) {
 }
 
 void ScavTrap::attack(const std::string &target) {
-	if (_energyPoints > 0) {
-		std::cout << "ScavTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
-		_energyPoints--;
-	} else {
-		std::cout << _name << " can't attack " << target << " any more." << std::endl;
-	}
+    if (_hitPoints == 0 || _energyPoints == 0)
+    {
+        std::cout << "ScavTrap " << _name << " cannot do anything because "
+                  << _name << " lacks hit points or energy points."
+                  << std::endl;
+        return;
+    }
+    std::cout << "ScavTrap " << _name << " attacks " << target << ", causing "
+              << _attackDamage << " points of damage!" << std::endl;
+    _energyPoints--;
 }
 
 void ScavTrap::guardGate() {
-	std::cout << "Gate keeper mode" << std::endl;
+	std::cout << "ScavTrap " << _name << " is Gate keeper mode" << std::endl;
 }
